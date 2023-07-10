b1cec956b5fa8d9cbb62d42186e0547788610d52
yigittaskin <taskinyigit84@gmail.com>
2023-07-10 22:48:03 +0300

    v1

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container mt-5 mb-5 p-3'>
+      <div style={{textAlign:'center', color: 'white'}}>
+        <h5><a style={{ color: '#848484' }} href='https://www.linkedin.com/in/taskinygt/' target='_blank' rel="noreferrer">Development by YGT</a></h5>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/TurkiyeMap.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import TurkeyMap from 'turkey-map-react'
+import './styles/turkeymap.css'
+
+const TurkiyeMap = () => {
+
+  const [country, setCountry] = useState([])
+
+  const addCountry = (newCountry) => {
+    setCountry([...country, newCountry])
+  }
+
+  const removeCountry = (countryName) => {
+    const updatedCountry = country.filter((c) => c !== countryName)
+    setCountry(updatedCountry);
+  }
+
+  const handleClick = (plateNumber, name, id) => {
+    const element = document.getElementById(`${id}`)
+    if (element.classList.length === 0) {
+      element.classList.add('selected')
+      addCountry(name)
+    } else {
+      element.classList.remove('selected')
+      removeCountry(name)
+    }
+  }
+
+  return (
+    <div>
+      <div className='container'>
+        <h2>Gezilen Şehir Sayısı: {country.length}/81</h2>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {
+            country.length > 0 ? <b>Gezdiğim Şehirler: </b> : ''
+          }
+          {
+            country.map((city) =>
+              <span key={city}>{city}</span>
+            )
+          }
+        </div>
+      </div>
+      <TurkeyMap
+        onClick={({ plateNumber, name, id }) => handleClick(plateNumber, name, id)}
+        hoverable={true}
+        showTooltip={true}
+      />
+    </div>
+  )
+}
+
+export default TurkiyeMap
